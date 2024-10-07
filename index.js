364390421dd92bf746aff7d0ed78bac640d458cb
unknown <noguchih@TDK-ZENSY.local>
2024-10-07 17:00:34 +0900

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
